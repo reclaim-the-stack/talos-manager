8f359e033dacebd0dd6e1df0bb0a68be329fbca5
David Backeus <duztdruid@gmail.com>
2023-02-17 16:23:31 +0100

    We're serious about SQLite

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # We're serious about SQLite3 in production
+  config.active_record.sqlite3_production_warning = false
 end
