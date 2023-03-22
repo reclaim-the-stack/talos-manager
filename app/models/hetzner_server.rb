cf45fe711b8fa88fd2c4f7b959d255699157a884
David Backeus <duztdruid@gmail.com>
2023-03-22 09:45:29 +0100

    Allow bootstrap to work on non-USB enabled servers

--- app/models/hetzner_server.rb
+++ app/models/hetzner_server.rb
@@ -23,14 +23,26 @@ class HetznerServer < ApplicationRecord
 
   def bootstrap!
     session = Net::SSH.start(ip, "root", key_data: [ENV.fetch("SSH_PRIVATE_KEY")])
-    uuid = session.exec! "dmidecode -s system-uuid"
+    uuid = session.exec! "dmidecode -s system-uuid".chomp
     if uuid.include? "-000000000000"
       system_data = session.exec! "dmidecode -t system"
       raise "Invalid SMBIOS UUID, send this output to Hetzner support: #{system_data}"
     end
 
-    ssh_exec_with_log! session, "wget '#{TALOS_ISO_URL}' -O talos.iso --no-verbose"
-    ssh_exec_with_log! session, "dd if=talos.iso of=/dev/sda status=progress"
+    # We order new servers with a USB device mounted on /dev/sda
+    if session.exec!("ls /dev/sda && echo 'has-sda'").chomp == "has-sda"
+      ssh_exec_with_log! session, "wget '#{TALOS_ISO_URL}' -O talos.iso --no-verbose"
+      ssh_exec_with_log! session, "dd if=talos.iso of=/dev/sda status=progress"
+    else
+      ssh_exec_with_log! session, "wget https://github.com/siderolabs/talos/releases/download/v1.3.6/metal-amd64.tar.gz"
+      ssh_exec_with_log! session, "tar xvfz metal-amd64.tar.gz" # expected file: disk.raw
+      ssh_exec_with_log! session, "dd if=disk.raw of=/dev/nvme0n1 status=progress"
+      ssh_exec_with_log! session, "sync"
+      # assuming that p3 is the BOOT partition, can make sure with `gdisk /dev/nvme0n1` and `s` command
+      ssh_exec_with_log! session, "mount /dev/nvme0n1p3 /mnt"
+      ssh_exec_with_log! session, 'sed -i \'s/vmlinuz/vmlinuz talos.config=https:\/\/devops-talos-manager.mynewsdesk.dev\/config?uuid=${uuid}/\' /mnt/grub/grub.cfg'
+      ssh_exec_with_log! session, "umount /mnt"
+    end
 
     begin
       session.exec! "reboot"
