8efc6f7c6be542213bd27d1536fdf8a660e8afee
David Backeus <duztdruid@gmail.com>
2023-04-14 18:58:50 +0200

    Only add basic authentication if ENV is set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   default_form_builder ApplicationFormBuilder
 
-  before_action :require_authentication
+  before_action :require_authentication if ENV["BASIC_AUTH_PASSWORD"].present?
 
   private
 
   def require_authentication
-    authenticate_or_request_with_http_basic do |username, password|
-      ENV["BASIC_AUTH_PASSWORD"].blank? || password == ENV.fetch("BASIC_AUTH_PASSWORD")
+    authenticate_or_request_with_http_basic do |_username, password|
+      password == ENV.fetch("BASIC_AUTH_PASSWORD")
     end
   end
 end
