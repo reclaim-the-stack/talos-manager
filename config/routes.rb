20ca9b1a9e7efef25943ec0edd05f35fa9ce9b38
David Backeus <duztdruid@gmail.com>
2023-04-17 09:44:08 +0200

    Move admin dashboards to /admin to enable Zero Trust rules

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,26 @@
 Rails.application.routes.draw do
-  resources :configs
-  resources :clusters do
-    member do
-      get :talosconfig
-      get :kubeconfig
+  scope "admin" do
+    resources :configs
+    resources :clusters do
+      member do
+        get :talosconfig
+        get :kubeconfig
+      end
     end
-  end
-  resources :machine_configs, only: %i[show new create destroy]
-  resources :servers, only: %i[index edit update] do
-    collection do
-      post :sync
-    end
+    resources :machine_configs, only: %i[show new create destroy]
+    resources :servers, only: %i[index edit update] do
+      collection do
+        post :sync
+      end
 
-    member do
-      post :bootstrap
-      post :rescue
+      member do
+        post :bootstrap
+        post :rescue
+      end
     end
   end
 
   get "config", to: "configs#show", as: "get_config"
 
-  root "servers#index"
+  root to: redirect("/admin/servers")
 end
