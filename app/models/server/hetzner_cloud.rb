d96ab5847b62dee08e0969d060590e6ee2f9c35a
David Backeus <duztdruid@gmail.com>
2023-04-17 15:53:48 +0200

    Improve handling of rescue lock

--- app/models/server/hetzner_cloud.rb
+++ app/models/server/hetzner_cloud.rb
@@ -2,13 +2,23 @@ class Server::HetznerCloud < Server
   def rescue
     ::HetznerCloud.active_rescue_system(id)
 
-    # Hetzner appears to temporarily lock the server immediately after enabling rescue mode
-    sleep 1
-
-    if ::HetznerCloud.server(id).fetch("status") == "off"
-      ::HetznerCloud.power_on(id)
-    else
-      ::HetznerCloud.reset(id)
+    # Hetzner appears to temporarily lock the server immediately after enabling rescue mode.
+    # Due to race conditions it's hard to detect this by fetching server status, so we just
+    # wait a bit and retry if a "server is locked" error is returned.
+    begin
+      if ::HetznerCloud.server(id).fetch("status") == "off"
+        ::HetznerCloud.power_on(id)
+      else
+        ::HetznerCloud.reset(id)
+      end
+    rescue ::HetznerCloud::HttpError => e
+      if e.message.include?("locked")
+        Rails.logger.info "Server #{id} is locked while enabling rescue mode, retrying in 1 second"
+        sleep 1
+        retry
+      else
+        raise
+      end
     end
   end
 
