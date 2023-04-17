89f9d57143dd172e6f0ed7a7a8fa9b00a3c6749f
David Backeus <duztdruid@gmail.com>
2023-04-17 10:05:35 +0200

    Fix require_authentication without password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   default_form_builder ApplicationFormBuilder
 
-  before_action :require_authentication if ENV["BASIC_AUTH_PASSWORD"].present?
+  before_action :require_authentication
 
   private
 
   def require_authentication
+    return if ENV["BASIC_AUTH_PASSWORD"].blank?
+
     authenticate_or_request_with_http_basic do |_username, password|
       password == ENV.fetch("BASIC_AUTH_PASSWORD")
     end
