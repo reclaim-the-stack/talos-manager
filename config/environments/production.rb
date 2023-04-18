539cbf4e48b19eab3c8ae4b6e64bb2e762d9ca39
David Backeus <duztdruid@gmail.com>
2023-04-18 17:21:27 +0200

    Add AssumeSSL middleware

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 require "active_support/core_ext/integer/time"
+require "middleware/assume_ssl"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -67,4 +68,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.middleware.insert_after ActionDispatch::HostAuthorization, AssumeSSL
 end
