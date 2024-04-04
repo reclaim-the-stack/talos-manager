74b8ff75f639ab4f987177ba85c79b4d334f48fd
David Backeus <duztdruid@gmail.com>
2024-04-04 13:39:02 +0200

    Remove support_unencrypted_data

--- config/application.rb
+++ config/application.rb
@@ -43,6 +43,5 @@ module DevopsTalosManager
     config.active_record.encryption.primary_key = ENV["AR_ENCRYPTION_PRIMARY_KEY"]
     config.active_record.encryption.deterministic_key = ENV["AR_ENCRYPTION_DETERMINISTIC_KEY"]
     config.active_record.encryption.key_derivation_salt = ENV["AR_ENCRYPTION_KEY_DERIVATION_SALT"]
-    config.active_record.encryption.support_unencrypted_data = true # TODO: remove this once we've migrated all data
   end
 end
