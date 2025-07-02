7c8f1e0dc82c7877fc91ae9da9202cd59c73fc2f
David Backeus <duztdruid@gmail.com>
2025-07-02 10:28:53 +0200

    Prevent Dell server product name to break the servers table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  # Goal here is to avoid bloating the width of the Model column in the servers table
+  def pretty_product(product)
+    # eg. Dell PowerEdge™ R6615 DX182
+    return product.split(" ").last if product.start_with?("Dell")
+
+    # Most products don't need this but it cleans up eg. AX41-NVMe
+    product.split("-").first
+  end
+
   # https://heroicons.com/
   def icon_edit
     %(<svg class="inline h-[18px]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" /></svg>).html_safe
